9bf0c8d20e4b98a1f4f13c3c1c0922af7e6d7221
Rahul Shakya <rahulshaky9653@gmail.com>
2026-09-06 22:43:38 +0530

    DSA day2.1

--- palindrome.java
+++ palindrome.java
@@ -1,15 +1,6 @@
 
 public class palindrome {
 
-    public static void main(String[] args) {
-
-        String one = "noon";
-        boolean result = isPalindrome(one);
-
-        System.out.println(result);
-
-    }
-
     public static boolean isPalindrome(String str) {
         for (int i = 0; i < str.length() / 2; i++) {
             int n = str.length();
@@ -18,6 +9,43 @@ public class palindrome {
             }
         }
         return true;
+    }
+
+    public static float getPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+
+            if (dir == 'S') {
+                y--;
+            } else if (dir == 'N') {
+                y++;
+            } else if (dir == 'W') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        int x2 = x * x;
+        int y2 = y * y;
+
+        return (float) Math.sqrt(x2 + y2);
+
+    }
+
+    public static String Substring(String arr, int si, int ei) {
+        String newArr = "";
+        for (int i = si; i < ei; i++) {
+            newArr += arr.charAt(i);
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        String arr = "hello world";
+
+        System.out.println("Substring: " + Substring(arr, 0, 4));
+
     }
 
 }
